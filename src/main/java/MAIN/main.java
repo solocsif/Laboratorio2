d2081e6a6228550db00d2ef0b9beb0ec838de5e0
javier <solocisf390@gmail.com>
2022-11-26 14:50:35 -0500

    Laboratorio 2 final

--- src/main/java/MAIN/main.java
+++ src/main/java/MAIN/main.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        System.out.println("Programa que calcula el MCM y MCD de maximo 3 numeros ingresados");
         System.out.println("Cuantos Numeros va a ingresar");
         int cantidadnumeros= scan.nextInt();
+        if ((cantidadnumeros<=0)||(cantidadnumeros>3)){
+            while ((cantidadnumeros<=0)||(cantidadnumeros>3)){
+                System.out.println("solo puede elejir ingresar 2 numeros maximo 3");
+                cantidadnumeros= scan.nextInt();
+            }
+        }
         int [][] mcm= new int[cantidadnumeros][];
         int [] numeros= new int[cantidadnumeros];
         int numerointeraccion = 0;
@@ -14,6 +21,7 @@ public class main {
         int contador=1;
         int columnas=0;
         int divisor=2;
+        int nummax=0;
         for (i=0;i<cantidadnumeros;i++){
             System.out.println("Ingrese su numero "+contador);
             int numeroingresado= scan.nextInt();
@@ -52,11 +60,56 @@ public class main {
             columnas=0;
         }
         for (int x=0;x<mcm.length;x++){
+            System.out.print("el numero: "+numeros[x]+" en Factores Primos es: ");
             for (int j=0;j<mcm[x].length;j++){
                 System.out.print(mcm[x][j]+" ");
             }
             System.out.println();
         }
+        nummax=numeros[0];
+        for (int x=0;x<numeros.length;x++){
+            if (numeros[x]>nummax){
+                nummax=numeros[x];
+            }
+        }
+        int mcmf=nummax;
+        if (cantidadnumeros==2){
+            while ((mcmf%numeros[0]!=0)||(mcmf%numeros[1]!=0)){
+                mcmf++;
+
+            }
+            System.out.println("el Minimo Comun Multiplo es: "+mcmf);
+        } else if (cantidadnumeros==3) {
+            while ((mcmf%numeros[0]!=0)||(mcmf%numeros[1]!=0)||(mcmf%numeros[2]!=0)){
+                mcmf++;
+
+            }
+            System.out.println("el Minimo Comun Multiplo es: "+mcmf);
+        }
+        int mcd=1;
+        if (cantidadnumeros==2){
+            while ((divisor<numeros[0])&&(divisor<numeros[1])){
+                while ((numeros[0]%divisor==0)&&(numeros[1]%divisor==0)){
+                    mcd=mcd*divisor;
+                    numeros[0]=numeros[0]/divisor;
+                    numeros[1]=numeros[1]/divisor;
+                }
+                divisor=divisor+1;
+            }
+            System.out.println("el Maximo Comun Divisor es: "+mcd);
+        } else if (cantidadnumeros==3) {
+            while ((divisor<numeros[0])&&(divisor<numeros[1])&&(divisor<numeros[2])){
+                while ((numeros[0]%divisor==0)&&(numeros[1]%divisor==0)&&(numeros[2]%divisor==0)){
+                    mcd=mcd*divisor;
+                    numeros[0]=numeros[0]/divisor;
+                    numeros[1]=numeros[1]/divisor;
+                    numeros[2]=numeros[2]/divisor;
+                }
+                divisor=divisor+1;
+            }
+            System.out.println("el Maximo Comun Divisor es: "+mcd);
+        }
+
 
     }
 }
